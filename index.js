97f612ac00fb47b41f087f9c0bd72bba86fd76b3
Anton777-crypto <avetrov392@gmail.com>
2024-11-01 16:21:37 +0000

    Deploying to gh-pages from @ Anton777-crypto/goit-js-hw-11@c97ff2c2ea58a73c3e0b826e24575adac4f9bb19 🚀

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import{a}from"./assets/vendor-C8zN0aMh.js";(function(){const o=document.createEl
               <p>Views: ${t.views}</p>
               <p>Comments: ${t.comments}</p>
             </div>
-          </div>`).join("");o.innerHTML=n}function d(){document.querySelector(".gallery").innerHTML=""}const m=document.querySelector("#search-form"),y=document.querySelector("#search-input");let i=1;const g=12;m.addEventListener("submit",async s=>{s.preventDefault();const o=y.value.trim();if(o){i=1,d();try{const n=await p(o,i,g);f(n.hits)}catch(n){console.error("Ошибка загрузки изображений:",n)}}});
+          </div>`).join("");o.innerHTML=n}function d(){document.querySelector(".gallery").innerHTML=""}const m=document.querySelector("#search-form"),y=document.querySelector("#search-input");let i=1;const g=12;m.addEventListener("submit",async s=>{s.preventDefault();const o=y.value.trim();if(o){i=1,d();try{const n=await p(o,i,g);f(n.hits)}catch(n){console.log("Ошибка загрузки изображений:",n)}}});
 //# sourceMappingURL=index.js.map
