5b2c70949eca8b533c68a0a91c5073f5c932fcb6
Anton2 Spaine <avetrov392@gmail.com>
2024-11-02 14:45:22 -0700

    add markup

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,27 +4,27 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 // pixabay-api.js
-// import axios from 'axios';
+import axios from 'axios';
 
-// const API_KEY = 'ВАШ_API_KEY'; // Вставьте свой ключ API от Pixabay
-// const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = 'ВАШ_API_KEY'; // Вставьте свой ключ API от Pixabay
+const BASE_URL = 'https://pixabay.com/api/';
 
-// export async function fetchImages(query, page = 1, perPage = 12) {
-//   try {
-//     const response = await axios.get(BASE_URL, {
-//       params: {
-//         key: API_KEY,
-//         q: query,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         page: page,
-//         per_page: perPage,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error('Ошибка запроса:', error);
-//     throw error;
-//   }
-// }
+export async function fetchImages(query, page = 1, perPage = 12) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: perPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Ошибка запроса:', error);
+    throw error;
+  }
+}
