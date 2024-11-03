3bc0c9c31a6b67180cc7c11c163cdf7f5a22978e
Anton2 Spaine <avetrov392@gmail.com>
2024-11-02 19:03:18 -0700

    add markup

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -11,7 +11,7 @@ const BAZ_URL = 'http://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, prePage = 12) {
   try {
-    const restor = await axios.get(BAZ_URL, {
+    const response = await axios.get(BAZ_URL, {
       params: {
         key: KEY,
         q: query,
@@ -19,12 +19,23 @@ export async function fetchImages(query, page = 1, prePage = 12) {
         orientation: 'horizontal',
         safesearch: true,
         page: page,
-        pre_Page: prePage,
+        pre_page: prePage,
       },
     });
-    return restor.data;
+
+    if (response.data.hits.length === 0) {
+      iziToast.error({
+        title: 'Помилка',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'center',
+        maxWidth: '250px ',
+        color: 'rgb(255, 162, 0)',
+      });
+    }
+    return response.data.hits;
   } catch (error) {
-    // console.log('Ошибка запроса', error);
+    console.log('Ошибка запроса:', error);
     iziToast.error({
       title: 'Помилка',
       message:
