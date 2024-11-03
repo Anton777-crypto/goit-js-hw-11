90d4e1727d2abb0eb488b776753009770efe707f
Anton2 Spaine <avetrov392@gmail.com>
2024-11-02 19:25:43 -0700

    add markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,23 +1,29 @@
-// render-functions.js
+render - functions.js;
 
-// export function renderImages(images) {
-//   const gallery = document.querySelector('.gallery');
-//   const markup = images
-//     .map(image => {
-//       return `
-//           <div class="gallery-item">
-//             <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-//             <div class="info">
-//               <p>Likes: ${image.likes}</p>
-//               <p>Views: ${image.views}</p>
-//               <p>Comments: ${image.comments}</p>
-//             </div>
-//           </div>`;
-//     })
-//     .join('');
-//   gallery.innerHTML = markup;
-// }
+export function renderImages(images) {
+  const gallery = document.querySelector('.gallery');
+  const markup = images
+    .map(image => {
+      return `
+          <div class="gallery-item">
+          <ul class='ul-gallery'>
+          <li class='li-gallery'>
+          <a>
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            </a>
+            <div class="info">
+              <p>Likes: ${image.likes}</p>
+              <p>Views: ${image.views}</p>
+              <p>Comments: ${image.comments}</p>
+              </div>
+              </li>
+            </ul>
+          </div>`;
+    })
+    .join('');
+  gallery.innerHTML = markup;
+}
 
-// export function clearGallery() {
-//   document.querySelector('.gallery').innerHTML = '';
-// }
+export function clearGallery() {
+  document.querySelector('.gallery').innerHTML = '';
+}
