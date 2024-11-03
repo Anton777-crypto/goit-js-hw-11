bd0894513a0bf581c512b7fa0ec1f267f5acaf03
Anton2 Spaine <avetrov392@gmail.com>
2024-11-02 19:21:19 -0700

    add markup

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-// // main.js
-// import { fetchImages } from './js/pixabay-api';
+// main.js
+import { fetchImages } from './js/pixabay-api';
 // import { renderImages, clearGallery } from './js/render-functions';
 
-// const form = document.querySelector('#search-form');
-// const input = document.querySelector('#search-input');
-// let page = 1;
-// const perPage = 12;
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#search-input');
+let page = 1;
+const perPage = 12;
 
-// form.addEventListener('submit', async event => {
-//   event.preventDefault();
-//   const query = input.value.trim();
-//   if (!query) return;
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  const query = input.value.trim();
+  if (!query) return;
 
-//   page = 1; // Сбрасываем номер страницы при новом поиске
-//   clearGallery(); // Очищаем предыдущие результаты
+  page = 1; // Сбрасываем номер страницы при новом поиске
+  clearGallery(); // Очищаем предыдущие результаты
 
-//   try {
-//     const data = await fetchImages(query, page, perPage);
-//     renderImages(data.hits);
-//   } catch (error) {
-//     console.log('Ошибка загрузки изображений:', error);
-//   }
-// });
+  try {
+    const data = await fetchImages(query, page, perPage);
+    renderImages(data.hits);
+  } catch (error) {
+    console.log('Ошибка загрузки изображений:', error);
+  }
+});
