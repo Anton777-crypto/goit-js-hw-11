a5ddece6ee796572c7032353e21c22d78b9e6055
Anton2 Spaine <avetrov392@gmail.com>
2024-11-05 18:41:52 -0800

    add markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,5 +1,7 @@
 // render - functions.js;
 
+import simpleLightbox from 'simplelightbox';
+
 export function renderImages(images) {
   const gallery = document.querySelector('.gallery');
   const markup = images
@@ -8,13 +10,15 @@ export function renderImages(images) {
     <div class="gallery-item">
          <ul class='ul-gallery'>
          <li class='li-gallery'>
-         <a  href='goit.com'>
+         <a  href='${image.largeImageURL}' data-lightbox = 'gallery'>
            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
            </a>
            <div class="info">
              <p>Likes: ${image.likes}</p>
              <p>Views: ${image.views}</p>
              <p>Comments: ${image.comments}</p>
+             <p>Comments: ${image.downloads}</p>
+
              </div>
              </li>
            </ul>
@@ -22,6 +26,11 @@ export function renderImages(images) {
     })
     .join('');
   gallery.innerHTML = markup;
+
+  const lightBox = new simpleLightbox('.gallary a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 }
 
 export function clearGallery() {
