3f6435669b7434467efca422e32b0b6fc72b41f7
Anton2 Spaine <avetrov392@gmail.com>
2024-11-05 18:57:29 -0800

    add markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,6 @@
 // render - functions.js;
-// Описаний у документації
-// Описаний у документації
-import SimpleLightbox from 'simplelightbox';
+
+import simpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
