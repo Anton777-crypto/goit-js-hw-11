0d722ad875571923e8d0c1361fe725ef0527ba29
Anton2 Spaine <avetrov392@gmail.com>
2024-11-10 16:04:12 -0800

    add markup

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,39 +1,50 @@
 // render - functions.js;
 
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt', // Подписи к изображениям берутся из атрибута alt
-  captionDelay: 250, // Задержка перед отображением подписи
-  animationSpeed: 300, // Скорость анимации при переходе
-  loop: true, // Включает циклический просмотр галереи
-});
-export function renderImages(images) {
-  const gallery = document.querySelector('.gallery');
-  const markup = images
-    .map(image => {
-      return `
-    <div class="gallery-item">
-         <ul class='ul-gallery'>
-         <li class='li-gallery'>
-         <a  href='${image.largeImageURL}' data-lightbox = 'gallery'>
-           <img class="images" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-           </a>
-           <div class="info">
-             <p class="info-info"><span class="info-p">Likes:</span>  ${image.likes}</p>
-             <p class="info-info"><span class="info-p">Views:</span> ${image.views}</p>
-             <p class="info-info"><span class="info-p">Comments:</span> ${image.comments}</p>
-             <p class="info-info"><span class="info-p">Comments:</span> ${image.downloads}</p>
 
-             </div>
-             </li>
-           </ul>
-         </div>`;
-    })
-    .join('');
-  gallery.innerHTML = markup;
-  lightbox.refresh();
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 300,
+  loop: true,
+});
+
+export async function renderImages(images) {
+  const loader = document.querySelector('.loader');
+  const gallery = document.querySelector('.gallery');
+
+  // Показать индикатор загрузки
+  loader.style.display = 'block';
+
+  try {
+    const markup = images
+      .map(image => {
+        return `
+          <div class="gallery-item">
+            <ul class='ul-gallery'>
+              <li class='li-gallery'>
+                <a href='${image.largeImageURL}' data-lightbox='gallery'>
+                  <img class="images" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+                </a>
+                <div class="info">
+                  <p class="info-info"><span class="info-p">Likes:</span> ${image.likes}</p>
+                  <p class="info-info"><span class="info-p">Views:</span> ${image.views}</p>
+                  <p class="info-info"><span class="info-p">Comments:</span> ${image.comments}</p>
+                  <p class="info-info"><span class="info-p">Downloads:</span> ${image.downloads}</p>
+                </div>
+              </li>
+            </ul>
+          </div>`;
+      })
+      .join('');
+
+    gallery.innerHTML = markup;
+
+    lightbox.refresh();
+  } finally {
+    loader.style.display = 'none';
+  }
 }
 
 export function clearGallery() {
